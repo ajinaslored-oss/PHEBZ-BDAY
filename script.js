0f6ff87d3c797f12fa354f0e265b79a103a9f2e3
simple <ajinaslored@gmail.com>
2026-09-21 13:49:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
    firefly name formation on the night screen.
 ========================================= */
 
-const BIRTHDAY_NAME = "Anuey";
+const BIRTHDAY_NAME = "Phebzzz";
 
 
 /* =========================================
